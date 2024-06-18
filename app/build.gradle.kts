43a564c68adb9833b8abd13c195b74dbc5fc4ce4
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-18 20:14:18 -0300

    refactor(images): removed patter images from Android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pajeapp"
+    namespace = "com.example.pageapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.pajeapp"
+        applicationId = "com.example.pageapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -59,6 +59,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
